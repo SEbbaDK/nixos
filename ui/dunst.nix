6246cae5b015068d5753cdf58f247efa4769504b
SEbbaDK <sebbadk@gmail.com>
2021-11-11 23:49:54 +0100

    ui: Add theming to dunst

--- ui/dunst.nix
+++ ui/dunst.nix
@@ -8,7 +8,24 @@ in
       enable = true;
       settings = {
         global = {
-          transparency = 20;
+          transparency = 50;
+
+          geometry = "300x0-10+10";
+          format = "<b>%s</b>\\n%b";
+          separator_height = 10;
+          padding = 10;
+          horizontal_padding = 10;
+          max_icon_size = 32;
+          word_wrap = "yes";
+
+          title = "Dunst";
+          class = "Dunst";
+
+          dmenu = "${pkgs.rofi}/bin/rofi -dmenu -p dunst";
+        };
+        normal = {
+          background = "#101010";
+          foreground = "#F0F0F0";
         };
       };
     };

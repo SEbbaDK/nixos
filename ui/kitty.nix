af33fb6298ec871a68db9274d1e49365ea3aaf01
SEbbaDK <sebbadk@gmail.com>
2021-11-11 12:56:45 +0100

    ui: Fix kitty ctrl+shift+ń opening a new window in same dir

--- ui/kitty.nix
+++ ui/kitty.nix
@@ -6,7 +6,7 @@ in
     enable = true;
     keybindings = {
       "kitty_mod+plus" = "change_font_size all +2.0";
-      "ctrl+shift+n" = "new_os_window --cwd=current";
+      "ctrl+shift+n" = "new_os_window_with_cwd";
     };
     settings = {
       font_size = 10;

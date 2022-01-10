6526973d64cb87dbae944e1dd525db2c92c5446c
SEbbaDK <sebbadk@gmail.com>
2022-01-10 18:15:45 +0100

    nas: Update NFS with new IPs

--- nas/nfs.nix
+++ nas/nfs.nix
@@ -13,8 +13,8 @@
 	services.nfs.server = {
     	enable = true;
     	exports = ''
-			/nfs/photos	10.0.0.0/24(insecure,nohide,rw,sync,no_subtree_check,no_root_squash)
-			/nfs/music	10.0.0.0/24(insecure,nohide,rw,sync,no_subtree_check,no_root_squash)
+			/nfs/photos	192.168.0.0/24(insecure,nohide,rw,sync,no_subtree_check,no_root_squash)
+			/nfs/music	192.168.0.0/24(insecure,nohide,rw,sync,no_subtree_check,no_root_squash)
     	'';
 	};
 }

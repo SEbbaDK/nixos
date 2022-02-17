9fc8aac8867feed058c074c742074306a1d1be63
SEbbaDK <sebbadk@gmail.com>
2022-02-17 23:20:09 +0100

    Add archive folder to NFS system

--- nas/nfs.nix
+++ nas/nfs.nix
@@ -9,12 +9,18 @@
         options = [ "bind" ];
     };
 
+    fileSystems."/nfs/archive" = {
+        device = "/mnt/big/archive";
+        options = [ "bind" ];
+    };
+
     networking.firewall.allowedTCPPorts = [ 2049 ];
 	services.nfs.server = {
     	enable = true;
     	exports = ''
-			/nfs/photos	192.168.0.0/24(insecure,nohide,rw,sync,no_subtree_check,no_root_squash)
-			/nfs/music	192.168.0.0/24(insecure,nohide,rw,sync,no_subtree_check,no_root_squash)
+			/nfs/photos  192.168.0.0/24(insecure,nohide,rw,sync,no_subtree_check,no_root_squash)
+			/nfs/music   192.168.0.0/24(insecure,nohide,rw,sync,no_subtree_check,no_root_squash)
+			/nfs/archive 192.168.0.0/24(insecure,nohide,rw,sync,no_subtree_check,no_root_squash)
     	'';
 	};
 }

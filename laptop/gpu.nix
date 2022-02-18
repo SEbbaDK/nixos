9093dee473f38ebaf70a450580079931b4f37b4b
SEbbaDK <sebbadk@gmail.com>
2022-02-18 13:44:53 +0100

    laptop: Fix using wrong gpu-drivers

--- laptop/gpu.nix
+++ laptop/gpu.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.package = pkgs.linuxPackages.nvidia_x11; #config.boot.kernelPackages.nvidiaPackages.legacy390;
+  hardware.nvidia.package = pkgs.linuxPackages.nvidia_x11_legacy390; #config.boot.kernelPackages.nvidiaPackages.legacy390;
   hardware.nvidia.prime = {
     sync.enable = true;
     nvidiaBusId = "PCI:1:0:0";

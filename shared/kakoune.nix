22b8a6854efd4f26d7d7641dc0075394ed33cda1
SEbbaDK <sebbadk@gmail.com>
2022-04-28 22:04:22 +0200

    shared: Fix kakoune plugins and config breaking by making it homemanager based

--- shared/kakoune.nix
+++ shared/kakoune.nix
@@ -5,28 +5,35 @@ let
     name = "kakrc.kak";
     destination = "/share/kak/autoload/${name}";
     text = ''
-      eval %sh{ kak-lsp --kakoune -s $kak_session }
-      lsp-enable
+      add-highlighter global/ number-lines
+      add-highlighter global/ wrap
+      terminal_enable_mouse=false
+      terminal_change_colors=false
+      eval %sh{kak-lsp --kakoune -s $kak_session}
+      lsp-enable-window
     '';
   });
+  plugins = with pkgs.kakounePlugins; [
+    kakoune-buffers
+    fzf-kak
+    kak-lsp
+    pandoc-kak
+    tabs-kak
+    kakboard
+  ];
 in
 {
   environment.variables.EDITOR = "kak";
-  environment.systemPackages = [
-    (pkgs.kakoune.override {
-      plugins = with pkgs.kakounePlugins; [
-        config
-        kakoune-buffers
-        fzf-kak
-        kak-lsp
-        pandoc-kak
-        tabs-kak
-        kakboard
-      ];
-    })
-  ];
+
+  #environment.systemPackages = [
+  #  (pkgs.kakoune.override {
+  #    plugins = [ config ] ++ plugins;
+  #  })
+  #];
+
   home-manager.users.${user}.programs.kakoune = {
     enable = true;
+    plugins = plugins;
     config = {
       numberLines.enable = true;
       tabStop = 4;
@@ -41,5 +48,8 @@ in
         { name = "WinCreate"; option = ".*"; commands = "kakboard-enable"; }
       ];
     };
+    extraConfig = ''
+      eval %sh{kak-lsp --kakoune -s $kak_session}
+    '';
   };
 }

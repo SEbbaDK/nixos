629bf530bd5ee955c72ca283fbc3ee29c05b2eed
SEbbaDK <sebbadk@gmail.com>
2022-07-06 12:14:09 +0200

    ui&nas: Open ports for syncthing

--- nas/syncthing.nix
+++ nas/syncthing.nix
@@ -2,6 +2,8 @@ let
   user = import ../user.nix;
 in
 {
+  networking.firewall.allowedTCPPorts = [ 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 ];
   services.syncthing = {
     enable = true;
   };
--- ui/syncthing.nix
+++ ui/syncthing.nix
@@ -2,5 +2,7 @@ let
 	user = import ../user.nix;
 in
 {
-	home-manager.users.${user}.services.syncthing.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 ];
+  home-manager.users.${user}.services.syncthing.enable = true;
 }

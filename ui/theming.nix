374eb65084fa2cfc381bea2675cc963b793df5de
SEbbaDK <sebbadk@gmail.com>
2022-07-06 10:21:27 +0200

    ui: Make lightdm simple

--- ui/theming.nix
+++ ui/theming.nix
@@ -1,7 +1,12 @@
 {
 	qt5.style = "adwaita-dark";
 	qt5.platformTheme = "gtk";
-	services.xserver.displayManager.lightdm.greeters.gtk.theme.name = "adwaita-dark";
-	services.xserver.displayManager.lightdm.greeters.enso.theme.name = "adwaita-dark";
+	services.xserver.displayManager.lightdm.greeters = {
+    	#enso.enable = true;
+    	mini.enable = true;
+    	mini.user = import ../user.nix;
+    	gtk.theme.name = "adwaita-dark";
+		enso.theme.name = "adwaita-dark";
+	};
     environment.variables.GTK_THEME = "Adwaita:dark";
 }

59fc2d4d52b61b2d6f125e6de271e06d81448f14
Sebastian Hyberts <sebastian.hyberts@subaio.com>
2023-09-20 11:21:27 +0200

    shared: Add more cli packages

--- shared/packages.nix
+++ shared/packages.nix
@@ -1,32 +1,39 @@
 { pkgs, ... }:
 let
-	mypkgs = import ../pkgs { inherit pkgs; };
+  mypkgs = import ../pkgs { inherit pkgs; };
 in
 {
   #boot.kernelPackages = pkgs.linuxPackages_latest;
   environment.systemPackages = with pkgs; [
-    git
-    gh glab
-    vim_configurable
-    nodejs # We want nodejs because of vim plugins
-    gnumake cmake gcc
-    ripgrep pcre # Better grep (<3 -o1)
-    jq fx # Work with json on commandline
-    ranger
-    mediainfo
-    htop
-    xclip
-    unzip unrar
-    ntfs3g # NTFS support (mkfs.ntfs)
-    tabview # CSV viewer
-    youtube-dl
-    todo-txt-cli
+    bat
+    bluetuith
+    curl
+    fd
+    ripgrep
+    fzf
     gdb
-    wget whois dig djbdns # WEB and DNS utilities
-    usbutils
-    miraclecast # Cast to TV
-    #mypkgs.comma # Disabled because it was broken
-    mypkgs.barion
     gdu # Disk Usage
+    gh glab
+    git
+    gnumake cmake gcc
+    htop
+    jq fx # Work with json on commandline
+    mediainfo
+    miraclecast # Cast to TV
+    #mypkgs.barion
+    #mypkgs.comma # Disabled because it was broken
+    nodejs # We want nodejs because of vim plugins
+    ntfs3g # NTFS support (mkfs.ntfs)
+    ranger
+    ripgrep pcre # Better grep (<3 -o1)
+    tabview # CSV viewer
+    todo-txt-cli
+    unzip unrar
+    usbutils
+    vim_configurable
+    wget whois dig djbdns # WEB and DNS utilities
+    xclip
+    ydiff
+    yt-dlp
   ];
 }

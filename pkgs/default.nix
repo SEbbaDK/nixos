99df4a4445c5c3808e3002b68b9a0d2be353392d
Sebastian Hyberts <sebastian.hyberts@subaio.com>
2023-09-20 11:21:11 +0200

    pkgs: Update callPackage

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,22 +1,23 @@
-{ pkgs ? import <nixpkgs> { }
-, callPackage ? pkgs.callPackage
-}:
+{ pkgs ? import <nixpkgs> { } }:
+let
+  callPackage = path: (import path) { inherit pkgs; };
+in
 {
-  screenshot = callPackage ./screenshot.nix { };
-  nix-convert = callPackage ./nix-convert.nix { };
-  uppaal = callPackage ./nix-convert.nix { };
-  cdranger = callPackage ./cdranger.nix { };
-  stregsystem-old = callPackage ./stregsystem.nix { };
-  stregsystem = callPackage (fetchTarball "https://github.com/f-klubben/stregsystemet-cli/archive/master.tar.gz") { };
-  zathura-pywall = callPackage ./zathura-pywal.nix { };
+  screenshot = callPackage ./screenshot.nix;
+  nix-convert = callPackage ./nix-convert.nix;
+  uppaal = callPackage ./nix-convert.nix;
+  cdranger = callPackage ./cdranger.nix;
+  stregsystem-old = callPackage ./stregsystem.nix;
+  stregsystem = callPackage (fetchTarball "https://github.com/f-klubben/stregsystemet-cli/archive/master.tar.gz");
+  zathura-pywall = callPackage ./zathura-pywal.nix;
 
-  comma = callPackage (fetchTarball "https://github.com/Shopify/comma/archive/master.tar.gz") { };
+  comma = callPackage (fetchTarball "https://github.com/Shopify/comma/archive/master.tar.gz");
 
-  barion = callPackage (fetchTarball "https://github.com/sebbadk/barion/archive/master.tar.gz") { };
+  barion = callPackage (fetchTarball "https://github.com/sebbadk/barion/archive/master.tar.gz");
 
-  droidcam-obs-plugin = callPackage ./droidcam-obs-plugin.nix { };
-  droidcam-obs-plugin-blobby = callPackage ./droidcam-obs-plugin-blobby.nix { };
+  droidcam-obs-plugin = callPackage ./droidcam-obs-plugin.nix;
+  droidcam-obs-plugin-blobby = callPackage ./droidcam-obs-plugin-blobby.nix;
 
-  fira-code-iscript = callPackage ./fira-code-iscript.nix { };
-  fira-code-victor = callPackage ./fira-code-victor.nix { };
+  fira-code-iscript = callPackage ./fira-code-iscript.nix;
+  fira-code-victor = callPackage ./fira-code-victor.nix;
 }

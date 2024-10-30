51ed689b598a68556ccc3931b1743cc66b60e64a
SEbbaDK <sebbadk@gmail.com>
2024-10-30 11:29:19 +0100

    ui: Switch to pipewire

--- ui/common.nix
+++ ui/common.nix
@@ -5,7 +5,14 @@
       drivers = [ pkgs.hplip ];
   };
 
-  hardware.pulseaudio.enable = true;
+  #hardware.pulseaudio.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
 
   services.autorandr.enable = true;
 
--- ui/gaming.nix
+++ ui/gaming.nix
@@ -5,5 +5,5 @@
       driSupport32Bit = true; # Stopping steam error
       extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
   };
-  hardware.pulseaudio.support32Bit = true;
+  # hardware.pulseaudio.support32Bit = true;
 }
